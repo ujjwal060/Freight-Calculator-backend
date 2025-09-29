c7649add48f6034ebc244ab8e63c46f217e8828c
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-09-29 12:25:28 +0530

    add chnage

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   mobileNumber: { type: String, required: true, unique: true },
-  password: { type: String, required: false },
+  password: { type: String, required: true },
   otp: { type: String },
   otpExpiry: { type: Date },
   isVerified: { type: Boolean, default: false }
